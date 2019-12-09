926a85be294a98eec64b39b7e2dfb3fc7aacbd38
mobarak10 <mobarak.hossain.cse12@gmail.com>
2019-12-10 00:07:48 +0600

    add bootstrap add Navbar.vue component add Navbar route

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import JQuery from 'jquery'
+window.$ = window.JQuery = JQuery
+import 'popper.js'
+import 'bootstrap'
+import './assets/app.scss'
+
+Vue.component('Navbar', require('./components/Navbar.vue').default);
 
 Vue.config.productionTip = false
 
